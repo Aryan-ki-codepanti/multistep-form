0ac2f7ef6e11b635b30d0edacf38e51127bf466e
Aryan Sethi <aryan@Aryans-Mac.local>
2024-07-16 00:21:01 +0530

    added styles and state for step 2

--- src/Components/StepTwo/index.jsx
+++ src/Components/StepTwo/index.jsx
@@ -1,8 +1,86 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./StepTwo.css";
+import FormContext from "../../Context/Form/FormContext";
+import arcadeImg from "../../Images/icon-arcade.svg";
+import advancedImg from "../../Images/icon-advanced.svg";
+import proImg from "../../Images/icon-pro.svg";
 
 const StepTwo = () => {
-    return <div>StepTwo</div>;
+    const { plan, setPlan } = useContext(FormContext);
+
+    return (
+        <div className="StepTwo">
+            <h1>Select your plan</h1>
+            <p>You have the option of monthly or yearly billing</p>
+
+            <div className="type">
+                <div
+                    className={
+                        plan.type === "arcade"
+                            ? "type-card active"
+                            : "type-card"
+                    }
+                    onClick={e =>
+                        setPlan(prev => ({ ...prev, type: "arcade" }))
+                    }
+                >
+                    <img src={arcadeImg} alt="arcade-image" />
+
+                    <div className="type-info">
+                        <p>Arcade</p>
+                        {plan.billing === "monthly" ? (
+                            <span>$9/mo</span>
+                        ) : (
+                            <>
+                                <span>$90/yr</span> <span>2 months free</span>
+                            </>
+                        )}
+                    </div>
+                </div>
+                <div
+                    className={
+                        plan.type === "advanced"
+                            ? "type-card active"
+                            : "type-card"
+                    }
+                    onClick={e =>
+                        setPlan(prev => ({ ...prev, type: "advanced" }))
+                    }
+                >
+                    <img src={advancedImg} alt="advanced-image" />
+
+                    <div className="type-info">
+                        <p>Advanced</p>
+                        {plan.billing === "monthly" ? (
+                            <span>$12/mo</span>
+                        ) : (
+                            <>
+                                <span>$120/yr</span> <span>2 months free</span>
+                            </>
+                        )}
+                    </div>
+                </div>
+                <div
+                    className={
+                        plan.type === "pro" ? "type-card active" : "type-card"
+                    }
+                    onClick={e => setPlan(prev => ({ ...prev, type: "pro" }))}
+                >
+                    <img src={proImg} alt="pro-image" />
+                    <div className="type-info">
+                        <p>Pro</p>
+                        {plan.billing === "monthly" ? (
+                            <span>$15/mo</span>
+                        ) : (
+                            <>
+                                <span>$150/yr</span> <span>2 months free</span>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default StepTwo;
--- src/Context/Form/FormProvider.js
+++ src/Context/Form/FormProvider.js
@@ -5,12 +5,26 @@ import { FormOneDefault, FormOneErrorDefault } from "../../Utils/InitialStates";
 const FormProvider = ({ children }) => {
     // stepOne
     const [formOne, setFormOne] = useState(FormOneDefault);
-
     const [formOneError, setFormOneError] = useState(FormOneErrorDefault);
+
+    // step two
+    // type : arcade / advanced / pro
+    // billing monthly / yearly
+    const [plan, setPlan] = useState({
+        type: "arcade",
+        billing: "monthly"
+    });
 
     return (
         <FormContext.Provider
-            value={{ formOne, setFormOne, formOneError, setFormOneError }}
+            value={{
+                formOne,
+                setFormOne,
+                formOneError,
+                setFormOneError,
+                plan,
+                setPlan
+            }}
         >
             {children}
         </FormContext.Provider>
